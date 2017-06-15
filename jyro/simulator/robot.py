de2c43749bf891235340c2281ca1e2b5b56ae394
Doug Blank <doug.blank@gmail.com>
2017-06-15 12:09:38 -0400

    Robot.brain method takes the robot (so that lambda works)

--- jyro/simulator/robot.py
+++ jyro/simulator/robot.py
@@ -39,7 +39,7 @@ class Robot():
         self.useTrail = False
         self.addDevice(Speech())
 
-    def brain(self):
+    def brain(self, robot):
         pass
             
     def reset(self):
